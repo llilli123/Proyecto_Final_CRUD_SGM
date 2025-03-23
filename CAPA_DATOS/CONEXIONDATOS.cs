a2917729504ee61a444e61faadf9d96fa558c9e4
Jorge Cabrera <jorgegabc.gg123@outlook.com>
2025-03-22 23:19:12 -0400

    Version 0.1 Conexion a Datos - Comienzo del Programa

--- CAPA_DATOS/CONEXIONDATOS.cs
+++ CAPA_DATOS/CONEXIONDATOS.cs
@@ -1,7 +1,7 @@
 ﻿using Microsoft.Data.SqlClient;
 
 namespace CAPA_DATOS
-{
+{// conexion a la base de datos
     public class CONEXIONDATOS
     {
         private SqlConnection Conexion = new SqlConnection("Server=.; Database = Proyecto_Final_SGM; Integrated Security =true");
