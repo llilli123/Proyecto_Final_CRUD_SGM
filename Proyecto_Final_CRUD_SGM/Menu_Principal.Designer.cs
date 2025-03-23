f2f0e7d22514bdf75f9a3c2885af8bb1a346f59e
Young Legend <flopezcastro112324@gmail.com>
2025-03-23 00:44:05 -0400

    version 0.0.4 Realizacion del Menustrip Proncipal

--- Proyecto_Final_CRUD_SGM/Menu_Principal.Designer.cs
+++ Proyecto_Final_CRUD_SGM/Menu_Principal.Designer.cs
@@ -28,31 +28,136 @@
         /// </summary>
         private void InitializeComponent()
         {
-            button1 = new Button();
+            menuStrip1 = new MenuStrip();
+            cITASToolStripMenuItem = new ToolStripMenuItem();
+            gESTIONARToolStripMenuItem = new ToolStripMenuItem();
+            listadoToolStripMenuItem = new ToolStripMenuItem();
+            pacientesToolStripMenuItem = new ToolStripMenuItem();
+            gestionarToolStripMenuItem1 = new ToolStripMenuItem();
+            listadoToolStripMenuItem1 = new ToolStripMenuItem();
+            consultaToolStripMenuItem = new ToolStripMenuItem();
+            gestionToolStripMenuItem = new ToolStripMenuItem();
+            doctorToolStripMenuItem = new ToolStripMenuItem();
+            citasToolStripMenuItem1 = new ToolStripMenuItem();
+            listadoDeDoctoresToolStripMenuItem = new ToolStripMenuItem();
+            acercaDeToolStripMenuItem = new ToolStripMenuItem();
+            menuStrip1.SuspendLayout();
             SuspendLayout();
             // 
-            // button1
+            // menuStrip1
             // 
-            button1.Location = new Point(36, 35);
-            button1.Name = "button1";
-            button1.Size = new Size(94, 29);
-            button1.TabIndex = 0;
-            button1.Text = "button1";
-            button1.UseVisualStyleBackColor = true;
+            menuStrip1.ImageScalingSize = new Size(20, 20);
+            menuStrip1.Items.AddRange(new ToolStripItem[] { cITASToolStripMenuItem, pacientesToolStripMenuItem, consultaToolStripMenuItem, doctorToolStripMenuItem, acercaDeToolStripMenuItem });
+            menuStrip1.Location = new Point(0, 0);
+            menuStrip1.Name = "menuStrip1";
+            menuStrip1.Size = new Size(800, 28);
+            menuStrip1.TabIndex = 5;
+            menuStrip1.Text = "menuStrip1";
+            // 
+            // cITASToolStripMenuItem
+            // 
+            cITASToolStripMenuItem.DropDownItems.AddRange(new ToolStripItem[] { gESTIONARToolStripMenuItem, listadoToolStripMenuItem });
+            cITASToolStripMenuItem.Name = "cITASToolStripMenuItem";
+            cITASToolStripMenuItem.Size = new Size(55, 24);
+            cITASToolStripMenuItem.Text = "Citas";
+            // 
+            // gESTIONARToolStripMenuItem
+            // 
+            gESTIONARToolStripMenuItem.Name = "gESTIONARToolStripMenuItem";
+            gESTIONARToolStripMenuItem.Size = new Size(224, 26);
+            gESTIONARToolStripMenuItem.Text = "Gestionar";
+            // 
+            // listadoToolStripMenuItem
+            // 
+            listadoToolStripMenuItem.Name = "listadoToolStripMenuItem";
+            listadoToolStripMenuItem.Size = new Size(224, 26);
+            listadoToolStripMenuItem.Text = "Listado";
+            // 
+            // pacientesToolStripMenuItem
+            // 
+            pacientesToolStripMenuItem.DropDownItems.AddRange(new ToolStripItem[] { gestionarToolStripMenuItem1, listadoToolStripMenuItem1 });
+            pacientesToolStripMenuItem.Name = "pacientesToolStripMenuItem";
+            pacientesToolStripMenuItem.Size = new Size(84, 24);
+            pacientesToolStripMenuItem.Text = "Pacientes";
+            // 
+            // gestionarToolStripMenuItem1
+            // 
+            gestionarToolStripMenuItem1.Name = "gestionarToolStripMenuItem1";
+            gestionarToolStripMenuItem1.Size = new Size(224, 26);
+            gestionarToolStripMenuItem1.Text = "Gestionar";
+            // 
+            // listadoToolStripMenuItem1
+            // 
+            listadoToolStripMenuItem1.Name = "listadoToolStripMenuItem1";
+            listadoToolStripMenuItem1.Size = new Size(224, 26);
+            listadoToolStripMenuItem1.Text = "Listado";
+            // 
+            // consultaToolStripMenuItem
+            // 
+            consultaToolStripMenuItem.DropDownItems.AddRange(new ToolStripItem[] { gestionToolStripMenuItem });
+            consultaToolStripMenuItem.Name = "consultaToolStripMenuItem";
+            consultaToolStripMenuItem.Size = new Size(80, 24);
+            consultaToolStripMenuItem.Text = "Consulta";
+            // 
+            // gestionToolStripMenuItem
+            // 
+            gestionToolStripMenuItem.Name = "gestionToolStripMenuItem";
+            gestionToolStripMenuItem.Size = new Size(224, 26);
+            gestionToolStripMenuItem.Text = "Gestion";
+            // 
+            // doctorToolStripMenuItem
+            // 
+            doctorToolStripMenuItem.DropDownItems.AddRange(new ToolStripItem[] { citasToolStripMenuItem1, listadoDeDoctoresToolStripMenuItem });
+            doctorToolStripMenuItem.Name = "doctorToolStripMenuItem";
+            doctorToolStripMenuItem.Size = new Size(69, 24);
+            doctorToolStripMenuItem.Text = "Doctor";
+            // 
+            // citasToolStripMenuItem1
+            // 
+            citasToolStripMenuItem1.Name = "citasToolStripMenuItem1";
+            citasToolStripMenuItem1.Size = new Size(225, 26);
+            citasToolStripMenuItem1.Text = "Citas";
+            // 
+            // listadoDeDoctoresToolStripMenuItem
+            // 
+            listadoDeDoctoresToolStripMenuItem.Name = "listadoDeDoctoresToolStripMenuItem";
+            listadoDeDoctoresToolStripMenuItem.Size = new Size(225, 26);
+            listadoDeDoctoresToolStripMenuItem.Text = "Listado de Doctores";
+            // 
+            // acercaDeToolStripMenuItem
+            // 
+            acercaDeToolStripMenuItem.Name = "acercaDeToolStripMenuItem";
+            acercaDeToolStripMenuItem.Size = new Size(89, 24);
+            acercaDeToolStripMenuItem.Text = "Acerca de";
             // 
             // Menu_Principal
             // 
             AutoScaleDimensions = new SizeF(8F, 20F);
             AutoScaleMode = AutoScaleMode.Font;
             ClientSize = new Size(800, 450);
-            Controls.Add(button1);
+            Controls.Add(menuStrip1);
             Name = "Menu_Principal";
             Text = "Form2";
+            menuStrip1.ResumeLayout(false);
+            menuStrip1.PerformLayout();
             ResumeLayout(false);
+            PerformLayout();
         }
 
         #endregion
 
-        private Button button1;
+        private MenuStrip menuStrip1;
+        private ToolStripMenuItem cITASToolStripMenuItem;
+        private ToolStripMenuItem gESTIONARToolStripMenuItem;
+        private ToolStripMenuItem listadoToolStripMenuItem;
+        private ToolStripMenuItem pacientesToolStripMenuItem;
+        private ToolStripMenuItem gestionarToolStripMenuItem1;
+        private ToolStripMenuItem listadoToolStripMenuItem1;
+        private ToolStripMenuItem consultaToolStripMenuItem;
+        private ToolStripMenuItem gestionToolStripMenuItem;
+        private ToolStripMenuItem doctorToolStripMenuItem;
+        private ToolStripMenuItem citasToolStripMenuItem1;
+        private ToolStripMenuItem listadoDeDoctoresToolStripMenuItem;
+        private ToolStripMenuItem acercaDeToolStripMenuItem;
     }
 }
