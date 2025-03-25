382357ac62ff81225aea60ff1143774db61c9adc
Dexx Encarnacion <dexxencarnacion20@hotmail.com>
2025-03-24 23:47:47 -0400

    0.0.7 Entrada de datos citas

---
+++ Proyecto_Final_CRUD_SGM/Entrada_de_datos_cita.Designer.cs
@@ -0,0 +1,427 @@
+﻿namespace CAPA_PRESENTACION
+{
+    partial class Entrada_de_datos_cita
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            panel1 = new Panel();
+            label12 = new Label();
+            label11 = new Label();
+            label10 = new Label();
+            panel2 = new Panel();
+            panel5 = new Panel();
+            textBox1 = new TextBox();
+            label1 = new Label();
+            panel3 = new Panel();
+            label2 = new Label();
+            label3 = new Label();
+            label4 = new Label();
+            label5 = new Label();
+            label6 = new Label();
+            textBox2 = new TextBox();
+            textBox3 = new TextBox();
+            textBox5 = new TextBox();
+            label7 = new Label();
+            label8 = new Label();
+            label9 = new Label();
+            textBox6 = new TextBox();
+            comboBox1 = new ComboBox();
+            radioButton1 = new RadioButton();
+            radioButton2 = new RadioButton();
+            checkBox1 = new CheckBox();
+            checkBox2 = new CheckBox();
+            checkBox3 = new CheckBox();
+            panel4 = new Panel();
+            button1 = new Button();
+            panel1.SuspendLayout();
+            panel5.SuspendLayout();
+            panel3.SuspendLayout();
+            panel4.SuspendLayout();
+            SuspendLayout();
+            // 
+            // panel1
+            // 
+            panel1.BackColor = SystemColors.Highlight;
+            panel1.Controls.Add(panel2);
+            panel1.Controls.Add(panel5);
+            panel1.Location = new Point(0, 0);
+            panel1.Name = "panel1";
+            panel1.Size = new Size(332, 722);
+            panel1.TabIndex = 0;
+            // 
+            // label12
+            // 
+            label12.AutoSize = true;
+            label12.Font = new Font("Segoe UI", 12F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            label12.ForeColor = SystemColors.ControlLightLight;
+            label12.Location = new Point(25, 67);
+            label12.Name = "label12";
+            label12.Size = new Size(262, 28);
+            label12.TabIndex = 6;
+            label12.Text = "informacion@grupouce.com";
+            // 
+            // label11
+            // 
+            label11.AutoSize = true;
+            label11.Font = new Font("Segoe UI", 12F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            label11.ForeColor = SystemColors.Control;
+            label11.Location = new Point(57, 39);
+            label11.Name = "label11";
+            label11.Size = new Size(160, 28);
+            label11.TabIndex = 5;
+            label11.Text = "(809)  4354 4343";
+            // 
+            // label10
+            // 
+            label10.AutoSize = true;
+            label10.Font = new Font("Segoe UI", 12F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            label10.ForeColor = SystemColors.ControlLightLight;
+            label10.Location = new Point(47, 11);
+            label10.Name = "label10";
+            label10.Size = new Size(193, 28);
+            label10.TabIndex = 4;
+            label10.Text = "Pongase en contacto";
+            // 
+            // panel2
+            // 
+            panel2.Location = new Point(363, 0);
+            panel2.Name = "panel2";
+            panel2.Size = new Size(250, 125);
+            panel2.TabIndex = 3;
+            // 
+            // panel5
+            // 
+            panel5.Controls.Add(label12);
+            panel5.Controls.Add(label10);
+            panel5.Controls.Add(label11);
+            panel5.Location = new Point(3, 492);
+            panel5.Name = "panel5";
+            panel5.Size = new Size(295, 125);
+            panel5.TabIndex = 7;
+            // 
+            // textBox1
+            // 
+            textBox1.Location = new Point(358, 143);
+            textBox1.Multiline = true;
+            textBox1.Name = "textBox1";
+            textBox1.Size = new Size(280, 45);
+            textBox1.TabIndex = 1;
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Font = new Font("Segoe UI", 16.2F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            label1.Location = new Point(6, 25);
+            label1.Name = "label1";
+            label1.Size = new Size(399, 38);
+            label1.TabIndex = 2;
+            label1.Text = "INFORMACION DE PACIENTE";
+            label1.Click += label1_Click;
+            // 
+            // panel3
+            // 
+            panel3.BackColor = SystemColors.ControlLightLight;
+            panel3.Controls.Add(label1);
+            panel3.Location = new Point(331, -1);
+            panel3.Name = "panel3";
+            panel3.Size = new Size(700, 85);
+            panel3.TabIndex = 3;
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Font = new Font("Segoe UI", 13.8F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            label2.Location = new Point(362, 109);
+            label2.Name = "label2";
+            label2.Size = new Size(114, 31);
+            label2.TabIndex = 4;
+            label2.Text = "* Nombre";
+            // 
+            // label3
+            // 
+            label3.AutoSize = true;
+            label3.Font = new Font("Segoe UI", 13.8F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            label3.Location = new Point(710, 107);
+            label3.Name = "label3";
+            label3.Size = new Size(116, 31);
+            label3.TabIndex = 5;
+            label3.Text = "* Apellido";
+            // 
+            // label4
+            // 
+            label4.AutoSize = true;
+            label4.Font = new Font("Segoe UI", 13.8F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            label4.Location = new Point(362, 224);
+            label4.Name = "label4";
+            label4.Size = new Size(217, 31);
+            label4.TabIndex = 6;
+            label4.Text = "* Correo Electronico";
+            // 
+            // label5
+            // 
+            label5.AutoSize = true;
+            label5.Font = new Font("Segoe UI", 13.8F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            label5.Location = new Point(362, 347);
+            label5.Name = "label5";
+            label5.Size = new Size(195, 31);
+            label5.TabIndex = 7;
+            label5.Text = "Cedula/Pasaporte";
+            // 
+            // label6
+            // 
+            label6.AutoSize = true;
+            label6.Font = new Font("Segoe UI", 13.8F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            label6.Location = new Point(370, 485);
+            label6.Name = "label6";
+            label6.Size = new Size(202, 31);
+            label6.TabIndex = 8;
+            label6.Text = "Cual es su seguro?";
+            // 
+            // textBox2
+            // 
+            textBox2.Location = new Point(358, 258);
+            textBox2.Multiline = true;
+            textBox2.Name = "textBox2";
+            textBox2.Size = new Size(280, 45);
+            textBox2.TabIndex = 9;
+            // 
+            // textBox3
+            // 
+            textBox3.Location = new Point(358, 381);
+            textBox3.Multiline = true;
+            textBox3.Name = "textBox3";
+            textBox3.Size = new Size(280, 49);
+            textBox3.TabIndex = 10;
+            // 
+            // textBox5
+            // 
+            textBox5.Location = new Point(706, 143);
+            textBox5.Multiline = true;
+            textBox5.Name = "textBox5";
+            textBox5.Size = new Size(272, 45);
+            textBox5.TabIndex = 12;
+            // 
+            // label7
+            // 
+            label7.AutoSize = true;
+            label7.Font = new Font("Segoe UI", 13.8F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            label7.Location = new Point(710, 224);
+            label7.Name = "label7";
+            label7.Size = new Size(116, 31);
+            label7.TabIndex = 13;
+            label7.Text = "* Telefono";
+            // 
+            // label8
+            // 
+            label8.AutoSize = true;
+            label8.Font = new Font("Segoe UI", 13.8F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            label8.Location = new Point(706, 347);
+            label8.Name = "label8";
+            label8.Size = new Size(178, 31);
+            label8.TabIndex = 14;
+            label8.Text = "Cual es su sexo?";
+            // 
+            // label9
+            // 
+            label9.AutoSize = true;
+            label9.Font = new Font("Segoe UI", 13.8F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            label9.Location = new Point(713, 485);
+            label9.Name = "label9";
+            label9.Size = new Size(140, 31);
+            label9.TabIndex = 15;
+            label9.Text = "Elegir Razon";
+            // 
+            // textBox6
+            // 
+            textBox6.Location = new Point(706, 258);
+            textBox6.Multiline = true;
+            textBox6.Name = "textBox6";
+            textBox6.Size = new Size(272, 45);
+            textBox6.TabIndex = 16;
+            // 
+            // comboBox1
+            // 
+            comboBox1.FormattingEnabled = true;
+            comboBox1.Location = new Point(367, 519);
+            comboBox1.Name = "comboBox1";
+            comboBox1.Size = new Size(280, 28);
+            comboBox1.TabIndex = 18;
+            // 
+            // radioButton1
+            // 
+            radioButton1.AutoSize = true;
+            radioButton1.Font = new Font("Segoe UI", 13.8F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            radioButton1.Location = new Point(713, 385);
+            radioButton1.Name = "radioButton1";
+            radioButton1.Size = new Size(140, 35);
+            radioButton1.TabIndex = 19;
+            radioButton1.TabStop = true;
+            radioButton1.Text = "Masculino";
+            radioButton1.UseVisualStyleBackColor = true;
+            // 
+            // radioButton2
+            // 
+            radioButton2.AutoSize = true;
+            radioButton2.Font = new Font("Segoe UI", 13.8F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            radioButton2.Location = new Point(713, 423);
+            radioButton2.Name = "radioButton2";
+            radioButton2.Size = new Size(135, 35);
+            radioButton2.TabIndex = 20;
+            radioButton2.TabStop = true;
+            radioButton2.Text = "Femenino";
+            radioButton2.UseVisualStyleBackColor = true;
+            // 
+            // checkBox1
+            // 
+            checkBox1.AutoSize = true;
+            checkBox1.Font = new Font("Segoe UI", 13.8F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            checkBox1.Location = new Point(710, 519);
+            checkBox1.Name = "checkBox1";
+            checkBox1.Size = new Size(275, 35);
+            checkBox1.TabIndex = 21;
+            checkBox1.Text = "Reflujo gastroesofágico";
+            checkBox1.UseVisualStyleBackColor = true;
+            // 
+            // checkBox2
+            // 
+            checkBox2.AutoSize = true;
+            checkBox2.Font = new Font("Segoe UI", 13.8F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            checkBox2.Location = new Point(710, 551);
+            checkBox2.Name = "checkBox2";
+            checkBox2.Size = new Size(236, 35);
+            checkBox2.TabIndex = 22;
+            checkBox2.Text = "Dispepsia funcional";
+            checkBox2.UseVisualStyleBackColor = true;
+            // 
+            // checkBox3
+            // 
+            checkBox3.AutoSize = true;
+            checkBox3.Font = new Font("Segoe UI", 13.8F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            checkBox3.Location = new Point(710, 581);
+            checkBox3.Name = "checkBox3";
+            checkBox3.Size = new Size(278, 35);
+            checkBox3.TabIndex = 23;
+            checkBox3.Text = "Úlcera gastro-duodenal";
+            checkBox3.UseVisualStyleBackColor = true;
+            // 
+            // panel4
+            // 
+            panel4.BackColor = SystemColors.ControlLightLight;
+            panel4.Controls.Add(button1);
+            panel4.Location = new Point(331, 634);
+            panel4.Name = "panel4";
+            panel4.Size = new Size(700, 87);
+            panel4.TabIndex = 24;
+            // 
+            // button1
+            // 
+            button1.BackColor = SystemColors.Highlight;
+            button1.BackgroundImageLayout = ImageLayout.None;
+            button1.Font = new Font("Segoe UI", 13.8F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            button1.ForeColor = SystemColors.HighlightText;
+            button1.Location = new Point(506, 15);
+            button1.Name = "button1";
+            button1.Size = new Size(179, 59);
+            button1.TabIndex = 0;
+            button1.Text = "Continuar";
+            button1.UseVisualStyleBackColor = false;
+            button1.Click += button1_Click;
+            // 
+            // Entrada_de_datos_cita
+            // 
+            AutoScaleDimensions = new SizeF(8F, 20F);
+            AutoScaleMode = AutoScaleMode.Font;
+            BackColor = Color.White;
+            ClientSize = new Size(1030, 721);
+            Controls.Add(panel4);
+            Controls.Add(checkBox3);
+            Controls.Add(checkBox2);
+            Controls.Add(checkBox1);
+            Controls.Add(radioButton2);
+            Controls.Add(radioButton1);
+            Controls.Add(comboBox1);
+            Controls.Add(textBox6);
+            Controls.Add(label9);
+            Controls.Add(label8);
+            Controls.Add(label7);
+            Controls.Add(textBox5);
+            Controls.Add(textBox3);
+            Controls.Add(textBox2);
+            Controls.Add(label6);
+            Controls.Add(label5);
+            Controls.Add(label4);
+            Controls.Add(label3);
+            Controls.Add(label2);
+            Controls.Add(panel3);
+            Controls.Add(textBox1);
+            Controls.Add(panel1);
+            Name = "Entrada_de_datos_cita";
+            Text = "Entrada_de_datos_cita";
+            Load += Entrada_de_datos_cita_Load;
+            panel1.ResumeLayout(false);
+            panel5.ResumeLayout(false);
+            panel5.PerformLayout();
+            panel3.ResumeLayout(false);
+            panel3.PerformLayout();
+            panel4.ResumeLayout(false);
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private Panel panel1;
+        private Panel panel2;
+        private TextBox textBox1;
+        private Label label1;
+        private Panel panel3;
+        private Label label2;
+        private Label label3;
+        private Label label4;
+        private Label label5;
+        private Label label6;
+        private TextBox textBox2;
+        private TextBox textBox3;
+        private TextBox textBox5;
+        private Label label7;
+        private Label label8;
+        private Label label9;
+        private TextBox textBox6;
+        private ComboBox comboBox1;
+        private RadioButton radioButton1;
+        private RadioButton radioButton2;
+        private CheckBox checkBox1;
+        private CheckBox checkBox2;
+        private CheckBox checkBox3;
+        private Panel panel4;
+        private Button button1;
+        private Label label12;
+        private Label label11;
+        private Label label10;
+        private Panel panel5;
+    }
+}
---
+++ Proyecto_Final_CRUD_SGM/Entrada_de_datos_cita.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace CAPA_PRESENTACION
+{
+    public partial class Entrada_de_datos_cita : Form
+    {
+        public Entrada_de_datos_cita()
+        {
+            InitializeComponent();
+        }
+
+        private void Entrada_de_datos_cita_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
