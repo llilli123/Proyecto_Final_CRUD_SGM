8fdeae9646434d2bf71760494ed39ddf5cf7624b
Young Legend <flopezcastro112324@gmail.com>
2025-03-25 00:36:55 -0400

    Version 0.0.8 Entrada de datos del paciente

---
+++ Proyecto_Final_CRUD_SGM/Properties/Entrada_Datos_Pacientes.Designer.cs
@@ -0,0 +1,273 @@
+﻿namespace CAPA_PRESENTACION.Properties
+{
+    partial class Entrada_Datos_Pacientes
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            button1 = new Button();
+            button2 = new Button();
+            button3 = new Button();
+            button4 = new Button();
+            button5 = new Button();
+            button6 = new Button();
+            label1 = new Label();
+            label2 = new Label();
+            label3 = new Label();
+            label4 = new Label();
+            label5 = new Label();
+            label6 = new Label();
+            label7 = new Label();
+            textBox1 = new TextBox();
+            textBox2 = new TextBox();
+            textBox3 = new TextBox();
+            textBox4 = new TextBox();
+            textBox5 = new TextBox();
+            textBox6 = new TextBox();
+            textBox7 = new TextBox();
+            SuspendLayout();
+            // 
+            // button1
+            // 
+            button1.Location = new Point(17, 22);
+            button1.Name = "button1";
+            button1.Size = new Size(94, 33);
+            button1.TabIndex = 0;
+            button1.Text = "Buscar";
+            button1.UseVisualStyleBackColor = true;
+            // 
+            // button2
+            // 
+            button2.Location = new Point(517, 22);
+            button2.Name = "button2";
+            button2.Size = new Size(94, 33);
+            button2.TabIndex = 1;
+            button2.Text = "Agregar";
+            button2.UseVisualStyleBackColor = true;
+            // 
+            // button3
+            // 
+            button3.Location = new Point(317, 22);
+            button3.Name = "button3";
+            button3.Size = new Size(94, 33);
+            button3.TabIndex = 2;
+            button3.Text = "Guardar";
+            button3.UseVisualStyleBackColor = true;
+            // 
+            // button4
+            // 
+            button4.Location = new Point(117, 22);
+            button4.Name = "button4";
+            button4.Size = new Size(94, 33);
+            button4.TabIndex = 3;
+            button4.Text = "Modificar";
+            button4.UseVisualStyleBackColor = true;
+            // 
+            // button5
+            // 
+            button5.Location = new Point(417, 22);
+            button5.Name = "button5";
+            button5.Size = new Size(94, 33);
+            button5.TabIndex = 4;
+            button5.Text = "Cancelar";
+            button5.UseVisualStyleBackColor = true;
+            // 
+            // button6
+            // 
+            button6.Location = new Point(217, 22);
+            button6.Name = "button6";
+            button6.Size = new Size(94, 33);
+            button6.TabIndex = 5;
+            button6.Text = "Eliminar";
+            button6.UseVisualStyleBackColor = true;
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Location = new Point(25, 78);
+            label1.Name = "label1";
+            label1.Size = new Size(67, 20);
+            label1.TabIndex = 6;
+            label1.Text = "Nombre:";
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Location = new Point(25, 114);
+            label2.Name = "label2";
+            label2.Size = new Size(69, 20);
+            label2.TabIndex = 7;
+            label2.Text = "Apellido:";
+            // 
+            // label3
+            // 
+            label3.AutoSize = true;
+            label3.Location = new Point(26, 150);
+            label3.Name = "label3";
+            label3.Size = new Size(60, 20);
+            label3.TabIndex = 8;
+            label3.Text = "Genero:";
+            // 
+            // label4
+            // 
+            label4.AutoSize = true;
+            label4.Location = new Point(26, 192);
+            label4.Name = "label4";
+            label4.Size = new Size(70, 20);
+            label4.TabIndex = 9;
+            label4.Text = "Telefono:";
+            // 
+            // label5
+            // 
+            label5.AutoSize = true;
+            label5.Location = new Point(26, 235);
+            label5.Name = "label5";
+            label5.Size = new Size(131, 20);
+            label5.TabIndex = 10;
+            label5.Text = "Fecha Nacimiento:";
+            // 
+            // label6
+            // 
+            label6.AutoSize = true;
+            label6.Location = new Point(26, 278);
+            label6.Name = "label6";
+            label6.Size = new Size(101, 20);
+            label6.TabIndex = 11;
+            label6.Text = "Nacionalidad:";
+            // 
+            // label7
+            // 
+            label7.AutoSize = true;
+            label7.Location = new Point(26, 324);
+            label7.Name = "label7";
+            label7.Size = new Size(90, 20);
+            label7.TabIndex = 12;
+            label7.Text = "Documento:";
+            // 
+            // textBox1
+            // 
+            textBox1.Location = new Point(164, 75);
+            textBox1.Name = "textBox1";
+            textBox1.Size = new Size(227, 27);
+            textBox1.TabIndex = 13;
+            // 
+            // textBox2
+            // 
+            textBox2.Location = new Point(163, 111);
+            textBox2.Name = "textBox2";
+            textBox2.Size = new Size(227, 27);
+            textBox2.TabIndex = 14;
+            // 
+            // textBox3
+            // 
+            textBox3.Location = new Point(163, 147);
+            textBox3.Name = "textBox3";
+            textBox3.Size = new Size(101, 27);
+            textBox3.TabIndex = 15;
+            // 
+            // textBox4
+            // 
+            textBox4.Location = new Point(163, 189);
+            textBox4.Name = "textBox4";
+            textBox4.Size = new Size(148, 27);
+            textBox4.TabIndex = 16;
+            // 
+            // textBox5
+            // 
+            textBox5.Location = new Point(163, 232);
+            textBox5.Name = "textBox5";
+            textBox5.Size = new Size(101, 27);
+            textBox5.TabIndex = 17;
+            // 
+            // textBox6
+            // 
+            textBox6.Location = new Point(163, 275);
+            textBox6.Name = "textBox6";
+            textBox6.Size = new Size(125, 27);
+            textBox6.TabIndex = 18;
+            // 
+            // textBox7
+            // 
+            textBox7.Location = new Point(164, 321);
+            textBox7.Name = "textBox7";
+            textBox7.Size = new Size(125, 27);
+            textBox7.TabIndex = 19;
+            // 
+            // Entrada_Datos_Pacientes
+            // 
+            AutoScaleDimensions = new SizeF(8F, 20F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(633, 450);
+            Controls.Add(textBox7);
+            Controls.Add(textBox6);
+            Controls.Add(textBox5);
+            Controls.Add(textBox4);
+            Controls.Add(textBox3);
+            Controls.Add(textBox2);
+            Controls.Add(textBox1);
+            Controls.Add(label7);
+            Controls.Add(label6);
+            Controls.Add(label5);
+            Controls.Add(label4);
+            Controls.Add(label3);
+            Controls.Add(label2);
+            Controls.Add(label1);
+            Controls.Add(button6);
+            Controls.Add(button5);
+            Controls.Add(button4);
+            Controls.Add(button3);
+            Controls.Add(button2);
+            Controls.Add(button1);
+            Name = "Entrada_Datos_Pacientes";
+            Text = "Form1";
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private Button button1;
+        private Button button2;
+        private Button button3;
+        private Button button4;
+        private Button button5;
+        private Button button6;
+        private Label label1;
+        private Label label2;
+        private Label label3;
+        private Label label4;
+        private Label label5;
+        private Label label6;
+        private Label label7;
+        private TextBox textBox1;
+        private TextBox textBox2;
+        private TextBox textBox3;
+        private TextBox textBox4;
+        private TextBox textBox5;
+        private TextBox textBox6;
+        private TextBox textBox7;
+    }
+}
