c0dd63e8bdf03794462244fff2ccac4f394c596c
Young Legend <flopezcastro112324@gmail.com>
2025-04-02 22:43:00 -0400

    version 0.1.2 percha activo

--- Proyecto_Final_CRUD_SGM/Entrada_Datos_Pacientes.Designer.cs
+++ Proyecto_Final_CRUD_SGM/Entrada_Datos_Pacientes.Designer.cs
@@ -36,11 +36,9 @@
             button6 = new Button();
             label1 = new Label();
             label2 = new Label();
-            label3 = new Label();
             label4 = new Label();
             label5 = new Label();
             label6 = new Label();
-            label7 = new Label();
             textBox1 = new TextBox();
             textBox2 = new TextBox();
             textBox3 = new TextBox();
@@ -48,11 +46,19 @@
             textBox5 = new TextBox();
             textBox6 = new TextBox();
             textBox7 = new TextBox();
+            label8 = new Label();
+            label10 = new Label();
+            label9 = new Label();
+            label11 = new Label();
+            label12 = new Label();
+            dateTimePicker1 = new DateTimePicker();
+            panel1 = new Panel();
+            panel1.SuspendLayout();
             SuspendLayout();
             // 
             // button1
             // 
-            button1.Location = new Point(17, 22);
+            button1.Location = new Point(884, 272);
             button1.Name = "button1";
             button1.Size = new Size(94, 33);
             button1.TabIndex = 0;
@@ -61,7 +67,7 @@
             // 
             // button2
             // 
-            button2.Location = new Point(517, 22);
+            button2.Location = new Point(884, 327);
             button2.Name = "button2";
             button2.Size = new Size(94, 33);
             button2.TabIndex = 1;
@@ -70,16 +76,19 @@
             // 
             // button3
             // 
-            button3.Location = new Point(317, 22);
+            button3.BackColor = SystemColors.Highlight;
+            button3.Font = new Font("Segoe UI", 9F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            button3.ForeColor = SystemColors.ButtonHighlight;
+            button3.Location = new Point(112, 7);
             button3.Name = "button3";
-            button3.Size = new Size(94, 33);
+            button3.Size = new Size(138, 47);
             button3.TabIndex = 2;
-            button3.Text = "Guardar";
-            button3.UseVisualStyleBackColor = true;
+            button3.Text = "Guardar Paciente";
+            button3.UseVisualStyleBackColor = false;
             // 
             // button4
             // 
-            button4.Location = new Point(117, 22);
+            button4.Location = new Point(884, 141);
             button4.Name = "button4";
             button4.Size = new Size(94, 33);
             button4.TabIndex = 3;
@@ -88,16 +97,16 @@
             // 
             // button5
             // 
-            button5.Location = new Point(417, 22);
+            button5.Location = new Point(6, 10);
             button5.Name = "button5";
-            button5.Size = new Size(94, 33);
+            button5.Size = new Size(100, 41);
             button5.TabIndex = 4;
             button5.Text = "Cancelar";
             button5.UseVisualStyleBackColor = true;
             // 
             // button6
             // 
-            button6.Location = new Point(217, 22);
+            button6.Location = new Point(884, 209);
             button6.Name = "button6";
             button6.Size = new Size(94, 33);
             button6.TabIndex = 5;
@@ -107,120 +116,181 @@
             // label1
             // 
             label1.AutoSize = true;
-            label1.Location = new Point(25, 78);
+            label1.Font = new Font("Segoe UI", 10.2F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            label1.ForeColor = SystemColors.MenuText;
+            label1.Location = new Point(27, 116);
             label1.Name = "label1";
-            label1.Size = new Size(67, 20);
+            label1.Size = new Size(81, 23);
             label1.TabIndex = 6;
             label1.Text = "Nombre:";
             // 
             // label2
             // 
             label2.AutoSize = true;
-            label2.Location = new Point(25, 114);
+            label2.Font = new Font("Segoe UI", 10.2F, FontStyle.Bold);
+            label2.Location = new Point(491, 119);
             label2.Name = "label2";
-            label2.Size = new Size(69, 20);
+            label2.Size = new Size(83, 23);
             label2.TabIndex = 7;
             label2.Text = "Apellido:";
-            // 
-            // label3
-            // 
-            label3.AutoSize = true;
-            label3.Location = new Point(26, 150);
-            label3.Name = "label3";
-            label3.Size = new Size(60, 20);
-            label3.TabIndex = 8;
-            label3.Text = "Genero:";
             // 
             // label4
             // 
             label4.AutoSize = true;
-            label4.Location = new Point(26, 192);
+            label4.Font = new Font("Segoe UI", 10.2F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            label4.Location = new Point(491, 319);
             label4.Name = "label4";
-            label4.Size = new Size(70, 20);
+            label4.Size = new Size(83, 23);
             label4.TabIndex = 9;
             label4.Text = "Telefono:";
             // 
             // label5
             // 
             label5.AutoSize = true;
-            label5.Location = new Point(26, 235);
+            label5.Font = new Font("Segoe UI", 10.2F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            label5.Location = new Point(491, 219);
             label5.Name = "label5";
-            label5.Size = new Size(131, 20);
+            label5.Size = new Size(157, 23);
             label5.TabIndex = 10;
             label5.Text = "Fecha Nacimiento:";
             // 
             // label6
             // 
             label6.AutoSize = true;
-            label6.Location = new Point(26, 278);
+            label6.Font = new Font("Segoe UI", 10.2F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            label6.Location = new Point(491, 424);
             label6.Name = "label6";
-            label6.Size = new Size(101, 20);
+            label6.Size = new Size(120, 23);
             label6.TabIndex = 11;
             label6.Text = "Nacionalidad:";
             // 
-            // label7
-            // 
-            label7.AutoSize = true;
-            label7.Location = new Point(26, 324);
-            label7.Name = "label7";
-            label7.Size = new Size(90, 20);
-            label7.TabIndex = 12;
-            label7.Text = "Documento:";
-            // 
             // textBox1
             // 
-            textBox1.Location = new Point(164, 75);
+            textBox1.Location = new Point(27, 151);
             textBox1.Name = "textBox1";
-            textBox1.Size = new Size(227, 27);
+            textBox1.Size = new Size(352, 27);
             textBox1.TabIndex = 13;
             // 
             // textBox2
             // 
-            textBox2.Location = new Point(163, 111);
+            textBox2.Location = new Point(27, 458);
             textBox2.Name = "textBox2";
-            textBox2.Size = new Size(227, 27);
+            textBox2.Size = new Size(390, 27);
             textBox2.TabIndex = 14;
             // 
             // textBox3
             // 
-            textBox3.Location = new Point(163, 147);
+            textBox3.Location = new Point(491, 355);
             textBox3.Name = "textBox3";
-            textBox3.Size = new Size(101, 27);
+            textBox3.Size = new Size(250, 27);
             textBox3.TabIndex = 15;
             // 
             // textBox4
             // 
-            textBox4.Location = new Point(163, 189);
+            textBox4.Location = new Point(27, 251);
             textBox4.Name = "textBox4";
-            textBox4.Size = new Size(148, 27);
+            textBox4.Size = new Size(293, 27);
             textBox4.TabIndex = 16;
             // 
             // textBox5
             // 
-            textBox5.Location = new Point(163, 232);
+            textBox5.Location = new Point(491, 458);
             textBox5.Name = "textBox5";
-            textBox5.Size = new Size(101, 27);
+            textBox5.Size = new Size(174, 27);
             textBox5.TabIndex = 17;
             // 
             // textBox6
             // 
-            textBox6.Location = new Point(163, 275);
+            textBox6.Location = new Point(27, 355);
             textBox6.Name = "textBox6";
-            textBox6.Size = new Size(125, 27);
+            textBox6.Size = new Size(140, 27);
             textBox6.TabIndex = 18;
             // 
             // textBox7
             // 
-            textBox7.Location = new Point(164, 321);
+            textBox7.Location = new Point(491, 151);
             textBox7.Name = "textBox7";
-            textBox7.Size = new Size(125, 27);
+            textBox7.Size = new Size(360, 27);
             textBox7.TabIndex = 19;
+            // 
+            // label8
+            // 
+            label8.AutoSize = true;
+            label8.Font = new Font("Segoe UI", 15F, FontStyle.Bold);
+            label8.Location = new Point(27, 22);
+            label8.Name = "label8";
+            label8.Size = new Size(366, 35);
+            label8.TabIndex = 20;
+            label8.Text = "Sistema de Citas Hospitalarias";
+            // 
+            // label10
+            // 
+            label10.AutoSize = true;
+            label10.Font = new Font("Segoe UI", 10F);
+            label10.Location = new Point(27, 61);
+            label10.Name = "label10";
+            label10.Size = new Size(555, 23);
+            label10.TabIndex = 22;
+            label10.Text = "Ingrese los datos personales del paciente para su registro en el sistema.";
+            // 
+            // label9
+            // 
+            label9.AutoSize = true;
+            label9.Font = new Font("Segoe UI", 10.2F, FontStyle.Bold);
+            label9.Location = new Point(27, 216);
+            label9.Name = "label9";
+            label9.Size = new Size(103, 23);
+            label9.TabIndex = 23;
+            label9.Text = "Documento";
+            // 
+            // label11
+            // 
+            label11.AutoSize = true;
+            label11.Font = new Font("Segoe UI", 10.2F, FontStyle.Bold);
+            label11.Location = new Point(27, 316);
+            label11.Name = "label11";
+            label11.Size = new Size(67, 23);
+            label11.TabIndex = 24;
+            label11.Text = "Genero";
+            // 
+            // label12
+            // 
+            label12.AutoSize = true;
+            label12.Font = new Font("Segoe UI", 10.2F, FontStyle.Bold);
+            label12.Location = new Point(27, 421);
+            label12.Name = "label12";
+            label12.Size = new Size(157, 23);
+            label12.TabIndex = 25;
+            label12.Text = "Correo Electrónico";
+            // 
+            // dateTimePicker1
+            // 
+            dateTimePicker1.Location = new Point(491, 251);
+            dateTimePicker1.Name = "dateTimePicker1";
+            dateTimePicker1.Size = new Size(324, 27);
+            dateTimePicker1.TabIndex = 27;
+            // 
+            // panel1
+            // 
+            panel1.Controls.Add(button3);
+            panel1.Controls.Add(button5);
+            panel1.Location = new Point(735, 32);
+            panel1.Name = "panel1";
+            panel1.Size = new Size(253, 61);
+            panel1.TabIndex = 28;
             // 
             // Entrada_Datos_Pacientes
             // 
             AutoScaleDimensions = new SizeF(8F, 20F);
             AutoScaleMode = AutoScaleMode.Font;
-            ClientSize = new Size(633, 450);
+            ClientSize = new Size(1016, 596);
+            Controls.Add(panel1);
+            Controls.Add(dateTimePicker1);
+            Controls.Add(label12);
+            Controls.Add(label11);
+            Controls.Add(label9);
+            Controls.Add(label10);
+            Controls.Add(label8);
             Controls.Add(textBox7);
             Controls.Add(textBox6);
             Controls.Add(textBox5);
@@ -228,21 +298,18 @@
             Controls.Add(textBox3);
             Controls.Add(textBox2);
             Controls.Add(textBox1);
-            Controls.Add(label7);
             Controls.Add(label6);
             Controls.Add(label5);
             Controls.Add(label4);
-            Controls.Add(label3);
             Controls.Add(label2);
             Controls.Add(label1);
             Controls.Add(button6);
-            Controls.Add(button5);
             Controls.Add(button4);
-            Controls.Add(button3);
             Controls.Add(button2);
             Controls.Add(button1);
             Name = "Entrada_Datos_Pacientes";
             Text = "Form1";
+            panel1.ResumeLayout(false);
             ResumeLayout(false);
             PerformLayout();
         }
@@ -257,11 +324,9 @@
         private Button button6;
         private Label label1;
         private Label label2;
-        private Label label3;
         private Label label4;
         private Label label5;
         private Label label6;
-        private Label label7;
         private TextBox textBox1;
         private TextBox textBox2;
         private TextBox textBox3;
@@ -269,5 +334,12 @@
         private TextBox textBox5;
         private TextBox textBox6;
         private TextBox textBox7;
+        private Label label8;
+        private Label label10;
+        private Label label9;
+        private Label label11;
+        private Label label12;
+        private DateTimePicker dateTimePicker1;
+        private Panel panel1;
     }
 }
